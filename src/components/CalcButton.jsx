5c42ccf4e4c16449bae1d58acf698ecd63d3ade7
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-05-12 10:04:54 +0100

    added button component with shadow reactions

---
+++ src/components/CalcButton.jsx
@@ -0,0 +1,27 @@
+import { useRef } from "react";
+
+export default function CalcButton({ value, onClick }) {
+  const buttonRef = useRef(null);
+
+  return (
+    <button
+      ref={buttonRef}
+      className="p-4 bg-slate-200 m-1 shadow-md/20 rounded-md"
+      onClick={onClick}
+      onMouseDown={() => {
+        if (buttonRef.current) {
+          buttonRef.current.classList.remove("shadow-md/20");
+          buttonRef.current.classList.add("inset-shadow-sm/20");
+        }
+      }}
+      onMouseUp={() => {
+        if (buttonRef.current) {
+          buttonRef.current.classList.remove("inset-shadow-sm/20");
+          buttonRef.current.classList.add("shadow-md/20");
+        }
+      }}
+    >
+      {value}
+    </button>
+  );
+}
