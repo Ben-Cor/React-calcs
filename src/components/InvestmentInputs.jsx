05de7f9fd9e33b38843a5d3598de809765550fb3
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-05-12 11:50:14 +0100

    added to investment inputs

--- src/components/InvestmentInputs.jsx
+++ src/components/InvestmentInputs.jsx
@@ -9,7 +9,19 @@ const [userInput, setUserInput] = useState({});
         <form action="">
             <div>
                 <label htmlFor="initialInvestment">Intitial Investment</label>
-                <input type="number" required />
+                <input id="initialInvestment" name="initialInvestment" type="number" required />
+            </div>
+            <div>
+                <label htmlFor="annualInvestment">Annual Investment</label>
+                <input id="annualInvestment" name="annualInvestment" type="number" required />
+            </div>
+            <div>
+                <label htmlFor="interestRate">Interest Rate</label>
+                <input id="interestRate" name="interestRate" type="number" required />
+            </div>
+            <div>
+                <label htmlFor="years">Years</label>
+                <input id="years" name="years" type="number" required />
             </div>
         </form>
     </div>
