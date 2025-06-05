1a1a5ffc52417b38b89f45b436c88166fb213a5f
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-06-05 09:14:07 +0100

    routing added to app and nav updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,30 @@ import { useState } from 'react'
 import Header from './components/Header'
 import CalcBasic from './components/CalcBasic'
 import InvestmentCalc from './components/InvestmentCalc'
+import BAsicCalc from './pages/BasicCalc'
+import InvestCalcPage from './pages/InvestCalcPage'
+import Home from './pages/Home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='bg-gray-100 min-h-screen'>
-      <Header />
-      <CalcBasic />
-      <InvestmentCalc />
-    </div>
+    <BrowserRouter>
+
+      <div className='bg-gray-100 min-h-screen'>
+        <Header />
+        <CalcBasic />
+        <InvestmentCalc />
+      </div>
+
+      <Routes>
+        <Route path="/basiccalc" element={<BasicCalc />} />
+        <Route path="/investmentcalc" element={<InvestCalcPage />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+
+    </BrowserRouter>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,9 @@ export default function Header() {
         <h1 className="text-2xl font-bold">Calculators</h1>
         <nav>
             <ul className="flex space-x-8">
-            <li><a href="#basic" className="hover:underline">Basic Calculator</a></li>
-            <li><a href="#investment" className="hover:underline">Investment Calculator</a></li>
-            <li><a href="#mortgage" className="hover:underline">Mortgage Calculator</a></li>
+            <li><a href="/basiccalc" className="hover:underline">Basic Calculator</a></li>
+            <li><a href="/investmentcalc" className="hover:underline">Investment Calculator</a></li>
+            <li><a href="/" className="hover:underline">Home</a></li>
             </ul>
         </nav>
         </header>
