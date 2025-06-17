0356aefabc2a34b56d4e905d517fdbec2ba6997b
Ben-Cor <ben_cornell@hotmail.co.uk>
2025-06-17 09:34:02 +0100

    improved mobile responsiveness

--- src/components/InvestmentInputs.jsx
+++ src/components/InvestmentInputs.jsx
@@ -6,19 +6,19 @@ export default function InvestmentInputs({userInput, handleChangeInput, handleCl
             <form className="flex flex-col text-end justify-end items-end w-auto gap-2 p-4 md:text-start md:flex-row" action="">
                 <div className="flex flex-col align-center justify-center gap-1">
                     <label htmlFor="initialInvestment">Initial Investment</label>
-                    <input className="border-[1px] text-end w-[150px]" id="initialInvestment" name="initialInvestment" value={userInput.initialInvestment} onChange={(e) => {handleChangeInput(e.target.id, e.target.value)}} type="number" required />
+                    <input className="select-none touch-manipulation border-[1px] text-end w-[150px]" id="initialInvestment" name="initialInvestment" value={userInput.initialInvestment} onChange={(e) => {handleChangeInput(e.target.id, e.target.value)}} type="number" required />
                 </div>
                 <div className="flex flex-col align-center justify-center gap-1">
                     <label htmlFor="annualInvestment">Annual Investment</label>
-                    <input className="border-[1px] text-end w-[150px]" id="annualInvestment" name="annualInvestment" value={userInput.annualInvestment} onChange={(e) => {handleChangeInput(e.target.id, e.target.value)}} type="number" required />
+                    <input className="select-none touch-manipulation border-[1px] text-end w-[150px]" id="annualInvestment" name="annualInvestment" value={userInput.annualInvestment} onChange={(e) => {handleChangeInput(e.target.id, e.target.value)}} type="number" required />
                 </div>
                 <div className="flex flex-col align-center justify-center gap-1">
                     <label htmlFor="interestRate">Interest Rate</label>
-                    <input className="border-[1px] text-end w-[150px]" id="interestRate" name="interestRate" value={userInput.interestRate} onChange={(e) => {handleChangeInput(e.target.id, e.target.value)}} type="number" required />
+                    <input className="select-none touch-manipulation border-[1px] text-end w-[150px]" id="interestRate" name="interestRate" value={userInput.interestRate} onChange={(e) => {handleChangeInput(e.target.id, e.target.value)}} type="number" required />
                 </div>
                 <div className="flex flex-col align-center justify-center gap-1">
                     <label htmlFor="years">Years</label>
-                    <input className="border-[1px] text-end w-[150px]" id="years" name="years" value={userInput.years} onChange={(e) => {handleChangeInput(e.target.id, e.target.value)}} type="number" required />
+                    <input className="select-none touch-manipulation border-[1px] text-end w-[150px]" id="years" name="years" value={userInput.years} onChange={(e) => {handleChangeInput(e.target.id, e.target.value)}} type="number" required />
                 </div>
             </form>
             <div>
